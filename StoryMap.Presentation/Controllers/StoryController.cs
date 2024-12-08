e4880c245eb0750c4ba6e03204e0124e610aa182
Catalin Plesu <catalinhimself@gmail.com>
2024-12-08 17:22:55 +0200

    feat: create navbar

---
+++ StoryMap.Presentation/Controllers/StoryController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using StoryMap.Presentation.Models;
+
+namespace StoryMap.Presentation.Controllers;
+
+public class StoryController : Controller
+{
+    private readonly ILogger<StoryController> _logger;
+
+    public StoryController(ILogger<StoryController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
