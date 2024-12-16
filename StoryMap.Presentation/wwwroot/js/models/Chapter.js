e380bb9f9dc4377f9ce7bd5f12760d8dc7a2b943
Catalin Plesu <catalinhimself@gmail.com>
2024-12-16 11:53:42 +0200

    refactor: remove unused files

--- StoryMap.Presentation/wwwroot/js/models/Chapter.js
+++
@@ -1,7 +0,0 @@
-export default class Chapter {
-  constructor(id, title, timestamps) {
-    this.id = id;
-    this.title = title;
-    this.timestamps = timestamps; // Array of timestamps
-  }
-}
--- StoryMap.Presentation/wwwroot/js/models/Character.js
+++
@@ -1,7 +0,0 @@
-export default class Character {
-  constructor(id, name, details) {
-    this.id = id;
-    this.name = name;
-    this.details = details; // This could be a key-value object
-  }
-}
