fac72953f6dfdf07d0069a9cdbb275c51642620e
Catalin Plesu <catalinhimself@gmail.com>
2024-12-23 19:10:13 +0200

    fix: view mode right bar

--- StoryMap.Presentation/wwwroot/js/components/RightBar.js
+++ StoryMap.Presentation/wwwroot/js/components/RightBar.js
@@ -5,116 +5,117 @@ const CharactersTree = {
   view() {
     return m("div.card.mb-3", [
       m("ul.tree.characters#tree-characters", [
-        m("li", {
-          oncontextmenu: (e) => {
-            const actions = [
-              {
-                label: "➕ Add details",
-                onClick: (_) => {
-                  const key = prompt(
-                    "Enter detail property (e.g., health, race):",
-                  );
-                  const value = prompt("Enter detail value:");
-                  if (key && value) {
-                    const newDetail = { [key]: value };
-                    State.addDetailToBaseCharacter(newDetail);
-                  }
+        State.mode === "view" ? null :
+          m("li", {
+            oncontextmenu: (e) => {
+              const actions = [
+                {
+                  label: "➕ Add details",
+                  onClick: (_) => {
+                    const key = prompt(
+                      "Enter detail property (e.g., health, race):",
+                    );
+                    const value = prompt("Enter detail value:");
+                    if (key && value) {
+                      const newDetail = { [key]: value };
+                      State.addDetailToBaseCharacter(newDetail);
+                    }
+                  },
                 },
-              },
-              {
-                label: "❌ Cancel",
-                onClick: (_) => { },
-              },
-            ];
-            ContextMenu.show(e, actions);
-          },
-        }, [
-          m("span.toggle", [
-            m("i.bi.bi-chevron-right"),
-          ]),
-          "Template Character",
-          m("ul", [
-            ...State.base_character_details.map((detail, detailIndex) =>
-              m("li", {
-                oncontextmenu: (e) => {
-                  const actions = [
-                    {
-                      label: `✏️ Edit Key: ${Object.keys(detail)[0]}`,
-                      onClick: (_) => {
-                        const oldDetail = { ...detail };
-                        const key = Object.keys(oldDetail)[0];
-                        const value = oldDetail[key];
+                {
+                  label: "❌ Cancel",
+                  onClick: (_) => { },
+                },
+              ];
+              ContextMenu.show(e, actions);
+            },
+          }, [
+            m("span.toggle", [
+              m("i.bi.bi-chevron-right"),
+            ]),
+            "📋 Template Character",
+            m("ul", [
+              ...State.base_character_details.map((detail, detailIndex) =>
+                m("li", {
+                  oncontextmenu: (e) => {
+                    const actions = [
+                      {
+                        label: `✏️ Edit Key: ${Object.keys(detail)[0]}`,
+                        onClick: (_) => {
+                          const oldDetail = { ...detail };
+                          const key = Object.keys(oldDetail)[0];
+                          const value = oldDetail[key];
 
-                        const newKey = prompt("✏️ Edit detail property:", key);
+                          const newKey = prompt("✏️ Edit detail property:", key);
 
-                        if (newKey) {
-                          const newDetail = { [newKey]: value };
-                          State.updateDetailInBaseCharacter(
-                            detailIndex,
-                            newDetail,
-                          );
-                        }
+                          if (newKey) {
+                            const newDetail = { [newKey]: value };
+                            State.updateDetailInBaseCharacter(
+                              detailIndex,
+                              newDetail,
+                            );
+                          }
+                        },
                       },
-                    },
-                    {
-                      label: `✏️ Edit Value: ${detail[Object.keys(detail)[0]]}`,
-                      onClick: (_) => {
-                        const oldDetail = detail;
-                        const key = Object.keys(oldDetail)[0];
-                        const value = oldDetail[key];
-                        const newValue = prompt("✏️ Edit detail property:", value);
-                        if (newValue) {
-                          const newDetail = { [key]: newValue };
-                          State.updateDetailInBaseCharacter(
-                            detailIndex,
-                            newDetail,
-                          );
-                        }
+                      {
+                        label: `✏️ Edit Value: ${detail[Object.keys(detail)[0]]}`,
+                        onClick: (_) => {
+                          const oldDetail = detail;
+                          const key = Object.keys(oldDetail)[0];
+                          const value = oldDetail[key];
+                          const newValue = prompt("✏️ Edit detail property:", value);
+                          if (newValue) {
+                            const newDetail = { [key]: newValue };
+                            State.updateDetailInBaseCharacter(
+                              detailIndex,
+                              newDetail,
+                            );
+                          }
+                        },
                       },
-                    },
-                    {
-                      label: "🗑️ Delete",
-                      onClick: (_) => {
-                        State.removeDetailFromBaseCharacter(detailIndex);
+                      {
+                        label: "🗑️ Delete",
+                        onClick: (_) => {
+                          State.removeDetailFromBaseCharacter(detailIndex);
+                        },
                       },
-                    },
-                    {
-                      label: "❌ Cancel",
-                      onClick: (_) => { },
-                    },
-                  ];
-                  ContextMenu.show(e, actions);
-                },
-              }, [
-                m(
-                  "span",
-                  `${Object.keys(detail)[0]}: ${detail[Object.keys(detail)[0]]
-                  }`,
-                ),
-              ])
-            ),
-            m("li", [
-              m("button.btn.btn-outline-primary.add-detail-btn", {
-                onclick: () => {
-                  const key = prompt(
-                    "Enter detail property (e.g., health, race):",
-                  );
-                  const value = prompt("Enter detail value:");
-                  if (key && value) {
-                    const newDetail = { [key]: value };
-                    State.addDetailToBaseCharacter(newDetail);
-                  }
-                },
-              }, m("i.bi.bi-plus.add-detail-btn")),
+                      {
+                        label: "❌ Cancel",
+                        onClick: (_) => { },
+                      },
+                    ];
+                    ContextMenu.show(e, actions);
+                  },
+                }, [
+                  m(
+                    "span",
+                    `${Object.keys(detail)[0]}: ${detail[Object.keys(detail)[0]]
+                    }`,
+                  ),
+                ])
+              ),
+              m("li", [
+                m("button.btn.btn-outline-primary.add-detail-btn", {
+                  onclick: () => {
+                    const key = prompt(
+                      "Enter detail property (e.g., health, race):",
+                    );
+                    const value = prompt("Enter detail value:");
+                    if (key && value) {
+                      const newDetail = { [key]: value };
+                      State.addDetailToBaseCharacter(newDetail);
+                    }
+                  },
+                }, m("i.bi.bi-plus.add-detail-btn")),
+              ]),
             ]),
           ]),
-        ]),
 
         m("li", [
           m("span.toggle", [
             m("i.bi.bi-chevron-right"),
           ]),
-          "Characters",
+          "🧑 Characters",
           m("ul", [
             ...State.getLatestCharacterChanges().map((characterHelper) => {
               return m("li", [
@@ -197,7 +198,7 @@ const CharactersTree = {
                         onClick: (_) => { },
                       },
                     ];
-                    ContextMenu.show(e, actions, characterHelper);
+                    State.mode === "view" ? null : ContextMenu.show(e, actions, characterHelper);
                   },
                 }, [
                   m("span.toggle", [
@@ -283,7 +284,7 @@ const CharactersTree = {
                                 onClick: (_) => { },
                               },
                             ];
-                            ContextMenu.show(e, actions);
+                            State.mode === "view" ? null : ContextMenu.show(e, actions);
                           },
                           onclick: (e) => {
                             e.stopPropagation();
@@ -302,37 +303,39 @@ const CharactersTree = {
                         ]);
                       },
                     ),
-                  m("li", [
-                    m("button.btn.btn-outline-primary.add-detail-btn", {
-                      onclick: () => {
-                        const key = prompt(
-                          "Enter detail property (e.g., health, race):",
-                        );
-                        const value = prompt("Enter detail value:");
-                        if (key && value) {
-                          const newDetail = { [key]: value };
-                          State.addDetailToCharacter(
-                            characterHelper.characterIndex,
-                            characterHelper.latestStateIndex,
-                            newDetail,
+                  State.mode === "view" ? null :
+                    m("li", [
+                      m("button.btn.btn-outline-primary.add-detail-btn", {
+                        onclick: () => {
+                          const key = prompt(
+                            "Enter detail property (e.g., health, race):",
                           );
-                        }
-                      },
-                    }, m("i.bi.bi-plus.add-detail-btn")),
-                  ]),
+                          const value = prompt("Enter detail value:");
+                          if (key && value) {
+                            const newDetail = { [key]: value };
+                            State.addDetailToCharacter(
+                              characterHelper.characterIndex,
+                              characterHelper.latestStateIndex,
+                              newDetail,
+                            );
+                          }
+                        },
+                      }, m("i.bi.bi-plus.add-detail-btn")),
+                    ]),
                 ]),
               ]);
             }),
-            m("li", [
-              m("button.btn.btn-outline-primary", {
-                onclick: () => {
-                  const newCharacterName = prompt("Enter new character name:");
-                  if (newCharacterName) {
-                    State.addCharacter(newCharacterName);
-                  }
-                },
-              }, m("i.bi.bi-plus")),
-            ]),
+            State.mode === "view" ? null :
+              m("li", [
+                m("button.btn.btn-outline-primary", {
+                  onclick: () => {
+                    const newCharacterName = prompt("Enter new character name:");
+                    if (newCharacterName) {
+                      State.addCharacter(newCharacterName);
+                    }
+                  },
+                }, m("i.bi.bi-plus")),
+              ]),
           ]),
         ]),
       ]),
